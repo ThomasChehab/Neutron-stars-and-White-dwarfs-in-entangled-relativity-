17578d6257717a45c5152d35811b91e4b275bc3f
ThomasChehab <chehab.thomas@outlook.com>
2025-02-21 14:47:35 +0100

    ajout macro

---
+++ White_dwarf_codes_macro/Get_all_figures.py
@@ -0,0 +1,42 @@
+from TOV import *
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy.constants as cst
+import numpy as np
+import os
+import tqdm 
+
+c2 = cst.c**2
+PhiInit = 1
+PsiInit = 0
+option = 1
+radiusMax_in = 20000000
+radiusMax_out = 100000000
+Npoint = 50000
+log_active = False # change for True for seeing star's data
+lowest_density = 1e9 # kg/m-3
+highest_density = 1e13
+densities = np.linspace(np.log(lowest_density), np.log(highest_density), 249)
+densities = np.exp(densities)
+count = 0
+verify()
+for density in tqdm.tqdm(densities):
+    tov = TOV( density ,PsiInit, PhiInit, radiusMax_in, radiusMax_out, Npoint, option, True, log_active, count)
+    tov.ComputeTOV()    
+    tov.recover_star_radius()
+    tov.recover_hbar_star()
+    tov.hbar_into_txt(count)
+    tov.radius_into_txt(count)
+    tov.density_into_txt()
+    if count == 0:
+        # key, value = tov.obtain_key_value()
+        tov.save_var_latex(1, 2)
+    elif count == 248:
+        # # key, value = tov.obtain_key_value()
+        tov.save_var_latex(1, 2)
+    # else:
+    #     pass
+    count += 1
+
+tov.Plot_all_hbar()
+
